96d42f1c974b62c326b0642e35bcf6b01df3f9f7
Varun <varun.mehta@raweng.com>
2021-06-07 17:34:47 +0530

    networking code updated

--- Sources/Networking/Networking.swift
+++ Sources/Networking/Networking.swift
@@ -9,14 +9,23 @@ import Foundation
 import UIKit
 
 public class Networking {
-    
-    public func performNetworkRequest(url: String, completion: @escaping (Any?, String?) -> Void) {
+
+    public func performNetworkRequest(url: String, method: String = "GET", params: [String: Any]?, headers: [String: String]?, completion: @escaping (Any?, String?) -> Void) {
         guard let url = URL(string: url) else {
             return
         }
         var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        
+        request.httpMethod = method
+        if let params = params, params.count > 0, let body = try? JSONSerialization.data(withJSONObject: params, options: [])
+        {
+            request.httpBody = body
+        }
+        if let headers = headers, headers.count > 0 {
+            var allheaders = [String: String]()
+            for header in headers {
+                allheaders[header.key] = header.value
+            }
+        }
         let session = URLSession.shared
         session.dataTask(with: request) { (data, response, error) in
             completion(data, error?.localizedDescription)
